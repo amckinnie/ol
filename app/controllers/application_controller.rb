afc59af63af1a7b13b35e3de646c4db167694c4c
Andrew McKinnie <admckinnie@gmail.com>
2016-08-07 16:52:08 -0500

    Add basic 404 response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def render_not_found
+    render status: :not_found,
+           json: {
+             status: 404,
+             message: 'Object not found',
+           }
   end
 end
